9ffa624a7fb120cb6224eb145f1cf57e41d73581
Travis Hanna <thanna@atlassian.com>
2023-04-28 10:11:32 -0500

    Renamed StringQuery to QueryBuilder, which is more descriptive of its function and style

--- aql_test.go
+++ aql_test.go
@@ -6,31 +6,31 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStringQuery(t *testing.T) {
+func TestQueryBuilder(t *testing.T) {
 
 	for _, test := range []struct {
 		name     string
-		query    *StringQuery
+		query    *QueryBuilder
 		expected string
 	}{
 		{
 			"should generate a basic statement",
-			NewStringQuery().Equal("attribute", "value"),
+			NewQueryBuilder().Equal("attribute", "value"),
 			`attribute = "value"`,
 		},
 		{
 			"should generate a statement with a int",
-			NewStringQuery().Equal("number", 135),
+			NewQueryBuilder().Equal("number", 135),
 			`number = 135`,
 		},
 		{
 			"should group query together",
-			NewStringQuery().Group(NewStringQuery().Equal("value", "one").Or().Equal("value", "three")),
+			NewQueryBuilder().Group(NewQueryBuilder().Equal("value", "one").Or().Equal("value", "three")),
 			`( value = "one" OR value = "three" )`,
 		},
 		{
 			"should support more complex queries",
-			NewStringQuery().Equal("attribute", "value").
+			NewQueryBuilder().Equal("attribute", "value").
 				And().GtrEqualTo("len", "0").
 				And().LessEqualTo("len", "0").
 				And().In("thinger", []any{"one", "two"}).
@@ -40,14 +40,14 @@ func TestStringQuery(t *testing.T) {
 		},
 		{
 			"should support more or queries",
-			NewStringQuery().Equal("attribute", "value").
+			NewQueryBuilder().Equal("attribute", "value").
 				And().GtrEqualTo("len", "0").
 				Or().LessEqualTo("len", "0"),
 			`attribute = "value" AND len >= "0" OR len <= "0"`,
 		},
 		{
 			"should support literal strings like null",
-			NewStringQuery().Equal("attribute", "value").
+			NewQueryBuilder().Equal("attribute", "value").
 				And().NotEqual("recommended", Literal("null")),
 			`attribute = "value" AND recommended != null`,
 		},
